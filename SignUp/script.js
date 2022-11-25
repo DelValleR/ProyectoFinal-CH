b772ba5b089c8220cf70168e08c42f7f9b5bb232
Daniel Del Valle R <97266451+DelValleR@users.noreply.github.com>
2022-11-25 10:29:21 -0500

    correccion1

--- SignUp/script.js
+++ SignUp/script.js
@@ -1,33 +1,47 @@
-//Preentrega #1 de proyecto final
+// //Preentrega #1 de proyecto final
 
-function saludo() {
-  alert(
-    'Recuerda que tu usuario debe tener entre 2 y 9 caracteres y tu contraseña debe ser un numero del 1 al 100'
-  );
-}
-saludo();
+// function saludo() {
+//   alert(
+//     'Recuerda que tu usuario debe tener entre 2 y 9 caracteres y tu contraseña debe ser un numero del 1 al 100'
+//   );
+// }
+// saludo();
 
-let user = prompt('Ingrese su usuario: ');
-while (user.length < 2 || user.length > 10) {
-  alert('Usuario no válido');
-  user = prompt('Ingrese su usuario correctamente');
-}
-alert('Usuario valido');
+// let user = prompt('Ingrese su usuario: ');
+// while (user.length < 2 || user.length > 10) {
+//   alert('Usuario no válido');
+//   user = prompt('Ingrese su usuario correctamente');
+// }
+// alert('Usuario valido');
 
-let password = prompt('Ingrese su contraseña: ');
-let incorrecto = true;
+// let password = prompt('Ingrese su contraseña: ');
+// let incorrecto = true;
 
-while (incorrecto) {
-  if (password > 100) {
-    incorrecto = true;
-    alert('Ingresa un numero entre 1 y 100');
-    password = prompt('Ingrese una contraseña valida');
-  } else if (isNaN(password)) {
-    incorrecto = true;
-    alert('La contraseña debe ser un numero');
-    password = prompt('Ingrese una contraseña valida');
+// while (incorrecto) {
+//   if (password > 100) {
+//     incorrecto = true;
+//     alert('Ingresa un numero entre 1 y 100');
+//     password = prompt('Ingrese una contraseña valida');
+//   } else if (isNaN(password)) {
+//     incorrecto = true;
+//     alert('La contraseña debe ser un numero');
+//     password = prompt('Ingrese una contraseña valida');
+//   } else {
+//     incorrecto = false;
+//     alert('Ingresaste correctamente la contraseña');
+//   }
+// }
+
+function entrar() {
+  const user = document.getElementById('usuario').value;
+  const password = document.getElementById('contraseña').value;
+
+  if (user.length < 2 || user.length > 10) {
+    alert('Usuario no válido, Ingrese su usuario correctamente');
+  } else if (password.length < 6) {
+    alert('Contraseña no válida, ingrese correctamente la contraseña');
   } else {
-    incorrecto = false;
-    alert('Ingresaste correctamente la contraseña');
+    window.location.href = '../store/store.html';
+    localStorage.usuario = user;
   }
 }
--- store/script.js
+++ store/script.js
@@ -1,79 +1,81 @@
-let resumenCompra = [];
+// let resumenCompra = [];
 
-function cuenta(resumenCompra) {
-  let precioAcumulado = 0;
-  let resumenStr = '';
-  for (let i = 0; i < resumenCompra.length; i++) {
-    precioAcumulado +=
-      resumenCompra[i].cantidad * resumenCompra[i].camiseta.precio;
-    resumenStr +=
-      resumenCompra[i].cantidad +
-      ' camisetas color ' +
-      resumenCompra[i].camiseta.color +
-      '\n';
-  }
-  alert(
-    'Escogiste estas camisetas: \n' +
-      resumenStr +
-      'Con un precio total de: $' +
-      precioAcumulado
-  );
-}
+// function cuenta(resumenCompra) {
+//   let precioAcumulado = 0;
+//   let resumenStr = '';
+//   for (let i = 0; i < resumenCompra.length; i++) {
+//     precioAcumulado +=
+//       resumenCompra[i].cantidad * resumenCompra[i].camiseta.precio;
+//     resumenStr +=
+//       resumenCompra[i].cantidad +
+//       ' camisetas color ' +
+//       resumenCompra[i].camiseta.color +
+//       '\n';
+//   }
+//   alert(
+//     'Escogiste estas camisetas: \n' +
+//       resumenStr +
+//       'Con un precio total de: $' +
+//       precioAcumulado
+//   );
+// }
 
-class Camisetas {
-  constructor(id, color, precio) {
-    this.id = id;
-    this.color = color;
-    this.precio = precio;
-  }
-}
+// class Camisetas {
+//   constructor(id, color, precio) {
+//     this.id = id;
+//     this.color = color;
+//     this.precio = precio;
+//   }
+// }
 
-const camisas = {
-  1: new Camisetas(001, 'Rojo', 40000),
-  2: new Camisetas(002, 'Azul', 42000),
-  3: new Camisetas(003, 'Verde', 50000),
-  4: new Camisetas(004, 'Amarillo', 45000),
-};
+// const camisas = {
+//   1: new Camisetas(001, 'Rojo', 40000),
+//   2: new Camisetas(002, 'Azul', 42000),
+//   3: new Camisetas(003, 'Verde', 50000),
+//   4: new Camisetas(004, 'Amarillo', 45000),
+// };
 
-const Prendas = Object.entries(camisas);
+// const Prendas = Object.entries(camisas);
 
-const objeto = Prendas.map((object) => object[1]);
+// const objeto = Prendas.map((object) => object[1]);
 
-const Colores = objeto.map((el) => el.color);
+// const Colores = objeto.map((el) => el.color);
 
-while (true) {
-  let shirts = prompt(
-    'Elija un color de camiseta, estos son los disponibles: ' +
-      '\n1. ' +
-      Colores[0] +
-      '\n2. ' +
-      Colores[1] +
-      '\n3. ' +
-      Colores[2] +
-      '\n4. ' +
-      Colores[3] +
-      '\n' +
-      'Cuando haya escogido las camisetas que desee escriba OK'
-  );
+// while (true) {
+//   let shirts = prompt(
+//     'Elija un color de camiseta, estos son los disponibles: ' +
+//       '\n1. ' +
+//       Colores[0] +
+//       '\n2. ' +
+//       Colores[1] +
+//       '\n3. ' +
+//       Colores[2] +
+//       '\n4. ' +
+//       Colores[3] +
+//       '\n' +
+//       'Cuando haya escogido las camisetas que desee escriba OK'
+//   );
 
-  if (shirts.toLowerCase() == 'ok') {
-    cuenta(resumenCompra);
-    break;
-  }
-  if (shirts > Object.keys(camisas).length) {
-    alert('Ese color no esta disponible, seleccione otro');
-    continue;
-  } else {
-    prendaComprada = camisas[shirts];
-    alert(
-      `Escogiste la camiseta de color ${prendaComprada.color} que tiene un precio de $ ${prendaComprada.precio}`
-    );
-  }
+//   if (shirts.toLowerCase() == 'ok') {
+//     cuenta(resumenCompra);
+//     break;
+//   }
+//   if (shirts > Object.keys(camisas).length) {
+//     alert('Ese color no esta disponible, seleccione otro');
+//     continue;
+//   } else {
+//     prendaComprada = camisas[shirts];
+//     alert(
+//       `Escogiste la camiseta de color ${prendaComprada.color} que tiene un precio de $ ${prendaComprada.precio}`
+//     );
+//   }
 
-  let cantidad = Number(prompt('¿Cuantas camisetas desea comprar?'));
+//   let cantidad = Number(prompt('¿Cuantas camisetas desea comprar?'));
 
-  resumenCompra.push({
-    cantidad: cantidad,
-    camiseta: prendaComprada,
-  });
-}
+//   resumenCompra.push({
+//     cantidad: cantidad,
+//     camiseta: prendaComprada,
+//   });
+// }
+
+document.getElementById('Usuario').innerHTML = localStorage.usuario;
