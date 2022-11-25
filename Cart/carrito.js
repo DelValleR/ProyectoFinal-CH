569e312c490b81813b810c1b996635e0eb652650
Daniel Del Valle R <97266451+DelValleR@users.noreply.github.com>
2022-11-25 18:49:20 -0500

    R

--- Cart/carrito.js
+++ Cart/carrito.js
@@ -23,28 +23,52 @@ function cargarProductosCarrito() {
       const div = document.createElement('div');
       div.classList.add('carrito-producto');
       div.innerHTML = `
-                <img class="carrito-producto-imagen" src="${
-                  producto.imagen
-                }" alt="${producto.titulo}">
-                <div class="carrito-producto-titulo">
-                    <small>Título</small>
-                    <h3>${producto.titulo}</h3>
-                </div>
-                <div class="carrito-producto-cantidad">
-                    <small>Cantidad</small>
-                    <p>${producto.cantidad}</p>
-                </div>
-                <div class="carrito-producto-precio">
-                    <small>Precio</small>
-                    <p>$${producto.precio}</p>
-                </div>
-                <div class="carrito-producto-subtotal">
-                    <small>Subtotal</small>
-                    <p>$${producto.precio * producto.cantidad}</p>
-                </div>
-                <button class="carrito-producto-eliminar" id="${
-                  producto.id
-                }"><i class="bi bi-trash-fill"></i></button>
+      <div class="card rounded-3 mb-4">
+      <div class="card-body p-4">
+        <div class="row d-flex justify-content-between align-items-center">
+          <div class="col-md-2 col-lg-2 col-xl-2">
+            <img
+              src="${producto.imagen}"
+              class="carrito-producto-imagen img-fluid rounded-3"
+              alt="${producto.titulo}"
+            />
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-3">
+            <p class="lead fw-normal mb-2">${producto.titulo}</p>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+            <button
+              class="btn btn-link px-2"
+              onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+            >
+              <i class="bi bi-dash"></i>
+            </button>
+    
+            <input
+              id="form1"
+              min="0"
+              name="quantity"
+              value="2"
+              type="number"
+              class="form-control form-control-sm"
+            />
+    
+            <button
+              class="btn btn-link px-2"
+              onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+            >
+              <i class="bi bi-plus"></i>
+            </button>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+            <h5 class="mb-0">$499.00</h5>
+          </div>
+          <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+            <a href="#!" class="text-danger"><i class="bi bi-trash3"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
             `;
 
       contenedorCarritoProductos.append(div);
